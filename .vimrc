89fbe09d0ceb93257f96d270b4e0a9ebc8bdb6f9
mare85 <mare85mare@gmail.com>
2017-08-07 15:50:49 +0200

    adding missing files

--- .vimrc
+++ .vimrc
@@ -2,6 +2,8 @@ set tabstop=4
 set softtabstop=4
 set shiftwidth=4
 set noexpandtab
-set colorcolumn=100
-highlight ColorColumn ctermbg=darkgrey
+set colorcolumn=110
+highlight ColorColumn ctermbg=8
 let &path.="code,"
+autocmd vimenter * NERDTree
+
--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,13 @@
 #include <stdio>
 int main() 
 {
-	return 0;
+	std::unique_ptr<int*> retValPtr(new int[1]);
+	*retValPtr = 0;
+	std::cout<< "complete me and nerd tree working"
+		<< std::endl
+		<< "currently not working"
+		<< std::endl
+		<< "ret value: " << *retValPtr
+		<< std::endl;
+	return *retValPtr;
 }
